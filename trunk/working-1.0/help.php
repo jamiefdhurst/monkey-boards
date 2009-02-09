ae590b3d487bd0a7329770a85930147cd8da5208
zenithus <none@none>
2009-02-09 23:01:02 +0000

    [svn r7] Begin documenting current source for ready-editing. Completed edit.php, forum.php, help.php and index.php.
    --HG--
    branch : monkeyboards

--- trunk/working-1.0/help.php
+++ trunk/working-1.0/help.php
@@ -1,22 +1,35 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /help.php
+ * Display forum-based help
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+// Require standard start file
 require('include/start.inc.php');
 
-$query = sqlite_query($db, "SELECT * FROM emoticons", SQLITE_ASSOC);
+// Get the emoticons for display
+$query = sqlite_query($db, "
+		SELECT * 
+		FROM `emoticons`
+	", SQLITE_ASSOC);
 $smilies = sqlite_fetch_all($query);
 
+// With each smiley, remove duplicates (I think)
 foreach($smilies as $key => $smiley) {
 	$index = $smiley['title'];
-	if(isset($checked[$index])) {
+	if(isset($checked[$index]))
 		unset($smilies[$key]);
-	}
-	else {
+	else
 		$checked[$index] = 1;
-	}
 }
 
+// Display template
 $tpl->assign('smilies', $smilies);
-
 $tpl->display("help.tpl");
 
 ?>
--- trunk/working-1.0/index.php
+++ trunk/working-1.0/index.php
@@ -1,38 +1,64 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /index.php
+ * Main page, display all categories and forums
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+// Require standard start file
 require('include/start.inc.php');
 
-$query = sqlite_query($db, "SELECT * FROM forums", SQLITE_ASSOC);
+// Get all forums (for if there are no categories)
+$query = sqlite_query($db, "
+		SELECT * 
+		FROM `forums`
+	", SQLITE_ASSOC);
 $forums = sqlite_fetch_all($query);
 $tpl->assign('forums_count', count($forums));
 
-$query = sqlite_query($db, "SELECT * FROM categories", SQLITE_ASSOC);
+// Get all categories
+$query = sqlite_query($db, "
+		SELECT * 
+		FROM `categories`
+	", SQLITE_ASSOC);
 $categories = sqlite_fetch_all($query);
-
 $categories_count = count($categories);
 
+// Which each category...
 foreach($categories as $cat_key => $category) {
-
-$query = sqlite_query($db, "SELECT * FROM forums WHERE category = '".$category['id']."' ORDER BY id ASC", SQLITE_ASSOC);
-$forums = sqlite_fetch_all($query);
-
-$forums_count = count($forums);
-
-if($forums_count > 0) {
-	foreach($forums as $key => $forum) {
-		$forums[$key]['name'] = htmlentities($forums[$key]['name']);
-		$forums[$key]['blurb'] = htmlentities($forums[$key]['blurb']);
-		$forums[$key]['topics'] = count_topics($forums[$key]['id']);
-		$forums[$key]['posts'] = count_posts($forums[$key]['id']);
-		$forums[$key]['last_post'] = forum_last_post($forums[$key]['id']);
+	
+	// Get this category's forums
+	$query = sqlite_query($db, "
+			SELECT * 
+			FROM `forums` 
+			WHERE `category` = '{$category['id']}'
+			ORDER BY `id` ASC
+		", SQLITE_ASSOC);
+	$forums = sqlite_fetch_all($query);
+	$forums_count = count($forums);
+	
+	// If we have forums, sort them out
+	if($forums_count > 0) {
+		// Strip out HTML, get the last post and count the topics and posts
+		foreach($forums as $key => $forum) {
+			$forums[$key]['name'] = htmlentities($forums[$key]['name']);
+			$forums[$key]['blurb'] = htmlentities($forums[$key]['blurb']);
+			$forums[$key]['topics'] = count_topics($forums[$key]['id']);
+			$forums[$key]['posts'] = count_posts($forums[$key]['id']);
+			$forums[$key]['last_post'] = forum_last_post($forums[$key]['id']);
+		}
+		$categories[$cat_key]['forums'] = $forums;
 	}
-	$categories[$cat_key]['forums'] = $forums;
 }
 
-}
-
+// Sort out site info
 $site_info = site_info();
 
+// Sort out and display template
 $tpl->assign('categories_count', $categories_count);
 $tpl->assign('forums', $categories);
 $tpl->assign('site_info', $site_info);
