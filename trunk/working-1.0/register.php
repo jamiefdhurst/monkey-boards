03352f9b87db3cc60b2798b0bbbda609a60a4535
zenithus <none@none>
2009-02-12 22:47:29 +0000

    [svn r11] More commenting and sorting out updates.
    --HG--
    branch : monkeyboards

--- trunk/working-1.0/register.php
+++ trunk/working-1.0/register.php
@@ -1,52 +1,82 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /register.php
+ * Register a new user on the forums
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+ // Parse a string with str_replace
+ // << This needs removing, str_replace supports arrays! >>
 function parse($string, $parsed) {
-	foreach($parsed as $find => $replace) {
+	foreach($parsed as $find => $replace)
 		$string = str_replace($find, $replace, $string);
-	}
 	return($string);
 }
 
+// Require standard start file
 require('include/start.inc.php');
 
+// If we have POST data
 if(!empty($v_username) and !empty($v_email)) {
 
-	// validate username and email address entered
+	// Validate username and email address entered
 	validate_username($v_username);
 	validate_email($v_email); 
 
-	// generate password and hash it
+	// Generate password and hash it
 	$password = generate_password();
 	$hashed_password = sha1($password);
 
-	// check username or email address not already in use
-	$result = sqlite_query($db, "SELECT * FROM users WHERE username = '$v_username' OR email = '$v_email'", SQLITE_ASSOC);
+	// Check username or email address not already in use
+	$result = sqlite_query($db, "
+			SELECT username
+			FROM `users` 
+			WHERE `username` = '{$v_username}' 
+			OR email = '{$v_email}'
+		", SQLITE_ASSOC);
 	$user = sqlite_fetch_array($result);
-	if(is_array($user)) exit($strings['user_exists']);
+	if(is_array($user))
+		die($strings['user_exists']);
 
-	// parse subject and message for email
+	// Parse subject and message for email
 	$parsed = array(
-		'{site_title}' => $settings['site_title'],
-		'{username}' => $v_username,
-		'{password}' => $password
-	);
-
+			'{site_title}' 	=> 	$settings['site_title'],
+			'{username}' 	=> 	$v_username,
+			'{password}' 	=> 	$password
+		);
 	$subject = parse($strings['welcome_email_subject'], $parsed);
 	$message = parse($strings['welcome_email_message'], $parsed);
 
-	// mail the welcome message
-	$mail = mail($v_email, $subject, $message, 'From: '.$settings['email_from']);
-	if(!$mail) exit($strings['mail_error']);
+	// Register the new user
+	$register = sqlite_exec($db, "
+			INSERT INTO `users` 
+			VALUES (
+				'{$v_username}', 
+				'{$hashed_password}', 
+				'{$v_email}', 
+				'1', 
+				'0', 
+				'{$time}', 
+				NULL
+			)
+		");
+	if(!$register)
+		die($strings['db_error']);
 
-	// register the new user
-	$register = sqlite_exec($db, "INSERT INTO users VALUES ('$v_username', '$hashed_password', '$v_email', 1, 0, '$time', NULL)");
-	if(!$register) exit($strings['db_error']);
+	// Mail out the welcome message
+	$mail = mail($v_email, $subject, $message, 'From: ' . $settings['email_from']);
+	if(!$mail)
+		die($strings['mail_error']);
 
-	// output success message
-	exit($strings['register_success']);
+	// Output success message
+	// << Need a template here. >>
+	die($strings['register_success']);
 
 }
 
+// Display registration template
 $tpl->display("register.tpl");
-
-?>
--- trunk/working-1.0/rules.php
+++ trunk/working-1.0/rules.php
@@ -1,6 +1,18 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /rules.php
+ * Display forum rules
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+// Require standard start file
 require('include/start.inc.php');
+
+// Display rules template
 $tpl->display("rules.tpl");
 
 ?>
