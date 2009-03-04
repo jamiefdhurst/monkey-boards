f3be4bbe7c5c8179fd96e4c63bd74dd9c7a96c67
zenithus <none@none>
2009-03-04 15:01:51 +0000

    [svn r13] Updated more comments, beginning admin section with categories and forums.
    --HG--
    branch : monkeyboards

--- trunk/working-1.0/admin/categories.php
+++ trunk/working-1.0/admin/categories.php
@@ -1,49 +1,100 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /admin/categories.php
+ * Manage forum categories
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+// Require standard start file and admin authentication library
 require('../include/start.inc.php');
 require('../include/admin_auth.inc.php');
 
+// We have an action to perform
 if(!empty($v_action)) {
-
+	
+	// If we have the category, make sure we pull it
 	if(isset($v_category)) {
-		$query = sqlite_query($db, "SELECT * FROM categories WHERE id = '$v_category'", SQLITE_ASSOC);
+		$query = sqlite_query($db, "
+				SELECT * 
+				FROM `categories` 
+				WHERE `id` = '{$v_category}'
+			", SQLITE_ASSOC);
 		$category = sqlite_fetch_array($query);
+		// Assign category to template
 		$tpl->assign('category', $category);
 	}
-
+	
+	// Choose action
 	switch($v_action) {
+		// Add a category
 		case "add":
+			// We need the name to add the category
+			// << Need some way to specify error here. >>
 			if(isset($v_name)) {
-				if(!ereg("^[A-Za-z][0-9A-Za-z ]*$", $v_name)) exit('Invalid category name.');
-
-				$query = sqlite_query($db, "SELECT id FROM categories WHERE name = '$v_name' LIMIT 0, 1", SQLITE_ASSOC);
+				// Category name must be valid (begin with letter)
+				// << Use preg instead of ereg. >>
+				if(!ereg('^[A-Za-z][0-9A-Za-z ]*$', $v_name))
+					 die('Invalid category name.');
+				
+				// Make sure we don't have a category with this name already
+				$query = sqlite_query($db, "
+						SELECT `id` 
+						FROM `categories` 
+						WHERE `name` = '{$v_name}'
+						LIMIT 0, 1
+					", SQLITE_ASSOC);
 				$check = sqlite_fetch_single($query);
-
-				if(!empty($check)) exit('Category already exists with that name.');
-
-				$update = sqlite_exec($db, "INSERT INTO categories VALUES ( NULL, '$v_name' );");
+				if(!empty($check))
+					die('Category already exists with that name.');
+				
+				// Insert the new category and return
+				$update = sqlite_exec($db, "
+						INSERT INTO `categories` 
+						VALUES (
+							null, 
+							'{$v_name}'
+						)
+					");
 				header('Location: categories.php');
 			}
-		break;
+			break;
+		// Save a new category name
+		// << Error checking is inconsistent. New category checks if name is set. >>
 		case "save":
-			$update = sqlite_exec($db, "UPDATE categories SET name = '$v_name' WHERE id = '$v_category'");
+			$update = sqlite_exec($db, "
+					UPDATE `categories` 
+					SET 
+						`name` = '{$v_name}' 
+					WHERE `id` = '{$v_category}'
+				");
 			header('Location: categories.php');
-		break;
+			break;
+		// Remove a category
+		// << Doesn't handle forums beneath this category. >>
 		case "delete":
-			$update = sqlite_exec($db, "DELETE FROM categories WHERE id = '$v_category'");
+			$update = sqlite_exec($db, "
+					DELETE FROM `categories` 
+					WHERE `id` = '{$v_category}'
+				");
 			header('Location: categories.php');
-		break;
+			break;
 	}
+	// Assign the action
 	$tpl->assign('action', $v_action);
 }
 
-$query = sqlite_query($db, "SELECT * FROM categories", SQLITE_ASSOC);
+// Pull categories
+$query = sqlite_query($db, "
+		SELECT * 
+		FROM `categories`
+	", SQLITE_ASSOC);
 $categories = sqlite_fetch_all($query);
 
-$categories_count = count($categories);
-$tpl->assign('categories_count', $categories_count);
-
+// Assign everything and display template
+$tpl->assign('categories_count', count($categories));
 $tpl->assign('categories', $categories);
-$tpl->display("admin_categories.tpl");
-
-?>
+$tpl->display('admin_categories.tpl');
--- trunk/working-1.0/admin/forums.php
+++ trunk/working-1.0/admin/forums.php
@@ -1,81 +1,175 @@
 <?php
 
+/**
+ * Monkey Boards
+ * /admin/categories.php
+ * Manage forum categories
+ * 
+ * @version 1.0
+ * @author Jamie Hurst
+ */
+
+// Require standard start file and admin authentication library
 require('../include/start.inc.php');
 require('../include/admin_auth.inc.php');
 
+// If we have an action, handle it
 if(!empty($v_action)) {
-
+	
+	// Get the forum details if we have it
 	if(isset($v_forum)) {
-		$query = sqlite_query($db, "SELECT * FROM forums WHERE id = '$v_forum'", SQLITE_ASSOC);
+		$query = sqlite_query($db, "
+				SELECT * 
+				FROM `forums` 
+				WHERE `id` = '{$v_forum}'
+			", SQLITE_ASSOC);
 		$forum = sqlite_fetch_array($query);
+		// Assign to template
 		$tpl->assign('forum', $forum);
 	}
-
+	
+	// Action to choose
 	switch($v_action) {
+		// Add a new forum
 		case "add":
+			// We need a name and description
+			// << No error handling again. >>
 			if(isset($v_name) and isset($v_description)) {
+				// Lower the name
 				$id = strtolower($v_name);
-				$id = eregi_replace(' ', '', $id);
-				if(!ereg("^[A-Za-z][0-9A-Za-z ]*$", $v_name)) exit('Invalid forum name.');
+				// Replace spaces with nothing
+				$id = str_replace(' ', '', $id);
+				// Make sure name is valid
+				// << Use preg instead. >>
+				if(!ereg('^[A-Za-z][0-9A-Za-z ]*$', $v_name)) 
+					die('Invalid forum name.');
+				
+				// << This should be entitied when pulling out. >>
 				$v_description = htmlentities($v_description);
-
-				$query = sqlite_query($db, "SELECT id FROM forums WHERE id = '$id' LIMIT 0, 1", SQLITE_ASSOC);
+				
+				// Start forum checking
+				$query = sqlite_query($db, "
+						SELECT `id` 
+						FROM `forums` 
+						WHERE `id` = '{$id}'
+						LIMIT 0, 1
+					", SQLITE_ASSOC);
 				$check = sqlite_fetch_single($query);
-
+				
+				// << What exactly is this bit supposed to do? Check SQL schema. >>
 				$count = 1;
 				$orig_id = $id;
 				while($check == $id) {
 					$count++;
 					$id = $orig_id.$count;
 
-					$query = sqlite_query($db, "SELECT id FROM forums WHERE id = '$id' LIMIT 0, 1", SQLITE_ASSOC);
+					$query = sqlite_query($db, "
+							SELECT `id` 
+							FROM `forums` 
+							WHERE `id` = '{$id}'
+							LIMIT 0, 1
+						", SQLITE_ASSOC);
 					$check = sqlite_fetch_single($query);
 				}
-
-				$update = sqlite_exec($db, "INSERT INTO forums VALUES ('$id', $v_category, '$v_name', '$v_description');");
+				
+				// Add the new forum
+				$update = sqlite_exec($db, "
+						INSERT INTO `forums` 
+						VALUES (
+							'{$id}', 
+							'{$v_category}', 
+							'{$v_name}', 
+							'{$v_description}'
+						)
+					");
+				// Redirect back
 				header('Location: forums.php');
 			}
-		break;
+			break;
+		// Edit a forum name
 		case "save":
-			if($v_id !== $v_forum) {
-				$update = sqlite_exec($db, "UPDATE topics SET forum = '$v_id' WHERE forum = '$v_forum'");
-			}
-			$update = sqlite_exec($db, "UPDATE forums SET id = '$v_id', category = $v_category, name = '$v_name', blurb = '$v_description' WHERE id = '$v_forum'");
+			// New name, update all forum references
+			if($v_id != $v_forum)
+				$update = sqlite_exec($db, "
+						UPDATE `topics` 
+						SET 
+							`forum` = '{$v_id}'
+						WHERE `forum` = '{$v_forum}'
+					");
+			// Update the forum entry in the main table
+			$update = sqlite_exec($db, "
+					UPDATE `forums` 
+					SET 
+						`id` = '{$v_id}', 
+						`category` = '{$v_category}', 
+						`name` = '{$v_name}', 
+						`blurb` = '{$v_description}' 
+					WHERE `id` = '{$v_forum}'
+				");
+			// Redirect back
 			header('Location: forums.php');
-		break;
+			break;
+		// Delete a forum
 		case "delete":
-			$query = sqlite_query($db, "SELECT * FROM topics WHERE forum = '$v_forum'", SQLITE_ASSOC);
+			// Get forum details
+			$query = sqlite_query($db, "
+					SELECT * 
+					FROM `topics` 
+					WHERE `forum` = '{$v_forum}'
+				", SQLITE_ASSOC);
 			$topics = sqlite_fetch_all($query);
-
+			
+			// For each topic and all its posts, nuke them
 			foreach($topics as $topic) {
 				$id = $topic['id'];
-				$query = sqlite_query($db, "SELECT * FROM posts WHERE topic = '$id'", SQLITE_ASSOC);
+				$query = sqlite_query($db, "
+						SELECT * 
+						FROM `posts` 
+						WHERE `topic` = '{$id}'
+					", SQLITE_ASSOC);
 				$posts = sqlite_fetch_all($query);
 				foreach($posts as $post) {
-					$post_id = $post['id'];
-					$update = sqlite_exec($db, "DELETE FROM posts WHERE id = '$post_id'");
+					$update = sqlite_exec($db, "
+						DELETE FROM `posts` 
+						WHERE `id` = '{$post['id']}'
+					");
 				}
-				$update = sqlite_exec($db, "DELETE FROM topics WHERE id = '$id'");
+				$update = sqlite_exec($db, "
+						DELETE FROM `topics` 
+						WHERE `id` = '{$id}'
+					");
 			}
-			$update = sqlite_exec($db, "DELETE FROM forums WHERE id = '$v_forum'");
+			// Delete the forum
+			$update = sqlite_exec($db, "
+					DELETE FROM `forums` 
+					WHERE `id` = '{$v_forum}'
+				");
+			// Redirect back
 			header('Location: forums.php');
-		break;
+			break;
 	}
+	// Assign the action to the template
 	$tpl->assign('action', $v_action);
 }
 
-$query = sqlite_query($db, "SELECT * FROM categories", SQLITE_ASSOC);
+// Get all categories
+$query = sqlite_query($db, "
+		SELECT * 
+		FROM `categories`
+	", SQLITE_ASSOC);
 $categories = sqlite_fetch_all($query);
 
+// Get each category's forums
 foreach($categories as $key => $category) {
-	$query = sqlite_query($db, "SELECT * FROM forums WHERE category = '".$category['id']."'", SQLITE_ASSOC);
-	$categories[$key]['forums'] = sqlite_fetch_all($query);}
+	$query = sqlite_query($db, "
+			SELECT * 
+			FROM `forums` 
+			WHERE `category` = '{$category['id']}'
+		", SQLITE_ASSOC);
+	$categories[$key]['forums'] = sqlite_fetch_all($query);
+}
 
-
-$categories_count = count($categories);
-$tpl->assign('categories_count', $categories_count);
-
+// Assign template and display
+$tpl->assign('categories_count', count($categories));
 $tpl->assign('categories', $categories);
-$tpl->display("admin_forums.tpl");
-
-?>
+$tpl->display('admin_forums.tpl');
